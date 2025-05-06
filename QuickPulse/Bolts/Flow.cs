5c223d783538354920b376957c06ea4045f12c30
kilfour <kilfour@gmail.com>
2025-05-06 09:52:58 +0200

    DistinctValueInspector

--- QuickPulse/Bolts/Flow.cs
+++ QuickPulse/Bolts/Flow.cs
@@ -1,5 +1,3 @@
-
-
 namespace QuickPulse.Bolts
 {
     public delegate Cask<T> Flow<T>(Signal input);
--- QuickPulse/Diagnostics/Sinks/DistinctValueInspector.cs
+++ QuickPulse/Diagnostics/Sinks/DistinctValueInspector.cs
@@ -1,32 +1,32 @@
-// namespace QuickMGenerate.Diagnostics.Inspectors;
+namespace QuickPulse.Diagnostics.Sinks;
 
-// public class DistinctValueInspector<T> : IAmAnInspector
-// {
-//     public readonly HashSet<T> Seen = new();
+public class DistinctValueInspector<T> : IPulse
+{
+    public readonly HashSet<T> Seen = new();
 
-//     public bool HasSeen(T value)
-//     {
-//         return Seen.Contains(value);
-//     }
+    public bool HasSeen(T value)
+    {
+        return Seen.Contains(value);
+    }
 
-//     public bool AllTheseHaveBeenSeen(T[] values)
-//     {
-//         return values.All(a => Seen.Contains(a));
-//     }
+    public bool AllTheseHaveBeenSeen(T[] values)
+    {
+        return values.All(a => Seen.Contains(a));
+    }
 
-//     public bool HasValueThatSatisfies(Func<T, bool> predicate)
-//     {
-//         return Seen.Any(predicate);
-//     }
+    public bool HasValueThatSatisfies(Func<T, bool> predicate)
+    {
+        return Seen.Any(predicate);
+    }
 
-//     public bool SeenSatisfyEach(Func<T, bool>[] predicates)
-//     {
-//         return predicates.All(predicate => Seen.Any(value => predicate(value)));
-//     }
+    public bool SeenSatisfyEach(Func<T, bool>[] predicates)
+    {
+        return predicates.All(predicate => Seen.Any(value => predicate(value)));
+    }
 
-//     public void Log(Entry entry)
-//     {
-//         if (entry.Data is T t || entry.Data == null)
-//             Seen.Add((T?)entry.Data);
-//     }
-// }
+    public void Log(object data)
+    {
+        if (data is T || data == null)
+            Seen.Add((T?)data!);
+    }
+}
