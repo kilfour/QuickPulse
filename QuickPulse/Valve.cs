3e0a2d89bfffa0ba7d73eb0b45691b2ed8d432c9
kilfour <kilfour@gmail.com>
2025-07-19 13:37:30 +0200

    Valve.Closed()

--- QuickPulse/Valve.cs
+++ QuickPulse/Valve.cs
@@ -4,9 +4,9 @@ public record Valve
 {
     private bool open = false;
     private Valve(bool open) { this.open = open; }
-    public static Valve Install() { return new Valve(true); }
+    public static Valve Closed() => new(false);
+    public static Valve Install() => new(true);
     public void Open() { open = true; }
-    public void Close() { open = false; }
     public bool Passable() { var result = open; open = false; return result; }
-    public bool Restricted() { var result = !open; open = false; return result; }
+    public bool Restricted() => !Passable();
 }
