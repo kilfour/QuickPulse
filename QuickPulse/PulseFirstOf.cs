0b4623d2f1201d8ec1e96d876736d513d21e12ed
kilfour <kilfour@gmail.com>
2025-08-09 23:00:25 +0200

    overloading

--- QuickPulse/PulseFirstOf.cs
+++ QuickPulse/PulseFirstOf.cs
@@ -4,10 +4,18 @@ namespace QuickPulse;
 
 public static partial class Pulse
 {
-    public static Flow<Unit> FirstOf(params (Func<bool>, Func<Flow<Unit>>)[] data) =>
-        state =>
+    public static Flow<Unit> FirstOf(params (Func<bool> Predicate, Func<Flow<Unit>> FlowFactory)[] data) =>
+        s =>
         {
-            foreach (var item in data) if (item.Item1()) return item.Item2()(state);
-            return Cask.Empty(state);
+            foreach (var item in data) if (item.Predicate()) return item.FlowFactory()(s);
+            return Cask.Empty(s);
+        };
+
+    public static Flow<Unit> FirstOf<T, TBox>(params (Func<TBox, bool> Predicate, Func<Flow<Unit>> FlowFactory)[] data) =>
+        s =>
+        {
+            var box = s.GetTheBox<TBox>().Value;
+            foreach (var item in data) if (item.Predicate(box)) return item.FlowFactory()(s);
+            return Cask.Empty(s);
         };
 }
