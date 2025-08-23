b9c488ebd1cf925de57b779b91b0823ee76cdc70
kilfour <kilfour@gmail.com>
2025-08-23 05:56:25 +0200

    0.2.3

---
+++ QuickPulse.Tests/Spike_OnType.cs
@@ -0,0 +1,37 @@
+using QuickPulse.Arteries;
+
+namespace QuickPulse.Tests;
+
+public class Spike_OnType
+{
+    public abstract class Base { }
+    public class DerivedOne : Base { }
+    public class DerivedTwo : Base { }
+    public class DerivedThree : Base { }
+
+    [Fact]
+    public void Riffing()
+    {
+        var one =
+            from start in Pulse.Start<DerivedOne>()
+            from _ in Pulse.Trace("DerivedOne")
+            select start;
+        var two =
+            from start in Pulse.Start<DerivedTwo>()
+            from _ in Pulse.Trace("DerivedTwo")
+            select start;
+        var flow =
+            from input in Pulse.Start<Base>()
+            from _ in Pulse.ToFlow(one, input)
+            from __ in Pulse.ToFlow(two, input)
+            from ___ in Pulse.Trace(" ")
+            select input;
+        var text =
+            Signal.From(flow)
+                .SetArtery(TheString.Catcher())
+                .Pulse([new DerivedOne(), new DerivedTwo(), new DerivedThree()])
+                .GetArtery<Holden>()
+                .Whispers();
+        Assert.Equal("DerivedOne DerivedTwo  ", text);
+    }
+}
